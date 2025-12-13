c03a3d30ea5a1b07fb6fb628dbb9e6e386540d75
gitymmwmm <git.ymmwmm@gmail.com>
2025-12-14 00:11:54 +0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,27 @@ import glsl from "vite-plugin-glsl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), glsl()],
+  plugins: [
+    react(),
+    // https://github.com/UstymUkhman/vite-plugin-glsl?tab=readme-ov-file#default-options
+    glsl({
+      include: [
+        // Glob pattern, or array of glob patterns to import
+        "**/*.glsl",
+        "**/*.wgsl",
+        "**/*.vert",
+        "**/*.frag",
+        "**/*.vs",
+        "**/*.fs",
+      ],
+      exclude: undefined, // Glob pattern, or array of glob patterns to ignore
+      defaultExtension: "glsl", // Shader suffix to use when no extension is specified
+      warnDuplicatedImports: true, // Warn if the same chunk was imported multiple times
+      removeDuplicatedImports: false, // Automatically remove an already imported chunk
+      importKeyword: "#include", // Keyword used to import shader chunks
+      minify: false, // Minify/optimize output shader code
+      watch: true, // Recompile shader on change
+      root: "/", // Directory for root imports
+    }),
+  ],
 });
